df564942af34fa0debc23819fbe4a7ffbe919095
ruhul <mohammadruhul.amin@carsales.com.au>
2016-08-15 10:39:43 +1000

    make asset reader error safe

--- Src/Carsales.Web/Infrastructure/AssetMappers/AssetDataReader.cs
+++ Src/Carsales.Web/Infrastructure/AssetMappers/AssetDataReader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Bolt.Logger;
 using Bolt.Serializer;
 using Carsales.Web.Infrastructure.Attributes;
 
@@ -15,16 +16,27 @@ namespace Carsales.Web.Infrastructure.AssetMappers
     public class AssetDataReader : IAssetDataReader
     {
         private readonly ISerializer serializer;
+        private readonly ILogger logger;
 
-        public AssetDataReader(ISerializer serializer)
+        public AssetDataReader(ISerializer serializer, ILogger logger)
         {
             this.serializer = serializer;
+            this.logger = logger;
         }
 
         public IDictionary<string, AssetData> Read()
         {
-            var content = File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/webpack.assets.json");
-            return serializer.Deserialize<IDictionary<string, AssetData>>(content);
+            try
+            {
+                var content = File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/webpack.assets.json");
+                return serializer.Deserialize<IDictionary<string, AssetData>>(content);
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, e.Message);
+            }
+
+            return new Dictionary<string, AssetData>();
         }
     }
 }
