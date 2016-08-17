6ebb9cc33d6d894985e2377a01eda77678d776fe
ruhul <mohammadruhul.amin@carsales.com.au>
2016-08-17 23:35:38 +1000

    add mapping infrastructure based on automapper

---
+++ Src/Carsales.Web/Infrastructure/Mappers/Mapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Carsales.Web.Infrastructure.Attributes;
+
+namespace Carsales.Web.Infrastructure.Mappers
+{
+    public interface IMapper
+    {
+        TOutput Map<TOutput>(object input);
+    }
+
+    [AutoBindSingleton]
+    public class AutoMappedMapper : IMapper
+    {
+        public TOutput Map<TOutput>(object input)
+        {
+            return AutoMapper.Mapper.Map<TOutput>(input);
+        }
+    }
+}
