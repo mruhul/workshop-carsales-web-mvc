b16d9621c5763aafad1c1eaa97c9317a24cce7c3
ruhul <mohammadruhul.amin@carsales.com.au>
2016-08-17 21:32:23 +1000

    add cache infrastructure

---
+++ Src/Carsales.Web/Infrastructure/Cache/CacheLife.cs
@@ -0,0 +1,9 @@
+﻿namespace Carsales.Web.Infrastructure.Cache
+{
+    public static class CacheLife
+    {
+        public const string Short = "Short";
+        public const string Medium = "Medium";
+        public const string Aggressive = "Aggressive";
+    }
+}
