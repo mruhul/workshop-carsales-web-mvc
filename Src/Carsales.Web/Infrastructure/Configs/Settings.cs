0c745cb0bc8c51cf9a17302cdae8b12bc62342b4
ruhul <mohammadruhul.amin@carsales.com.au>
2016-08-17 20:20:05 +1000

    Add generic settings infrastructure

---
+++ Src/Carsales.Web/Infrastructure/Configs/Settings.cs
@@ -0,0 +1,32 @@
+﻿namespace Carsales.Web.Infrastructure.Configs
+{
+    public interface ISettings<out T> where T: class, new()
+    {
+        T Value { get; }
+    }
+
+    public abstract class SettingsBase<T> : ISettings<T> where T : class, new()
+    {
+        private static readonly object Lock = new object();
+        private static T settings;
+
+        public T Value
+        {
+            get
+            {
+                if (settings != null) return settings;
+
+                lock (Lock)
+                {
+                    if (settings != null) return settings;
+
+                    settings = SettingsHelper.GetSettings<T>(SectionName);
+
+                    return settings;
+                }
+            }
+        }
+
+        protected abstract string SectionName { get; }
+    }
+}
---
+++ Src/Carsales.Web/Infrastructure/Configs/SettingsHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+
+namespace Carsales.Web.Infrastructure.Configs
+{
+    public static class SettingsHelper
+    {
+        public static T GetSettings<T>(string sectionName) where T : new()
+        {
+            var configSection = ConfigurationManager.GetSection(sectionName) as NameValueCollection;
+
+            if (configSection == null) throw new ConfigurationErrorsException($"Failed to load PlainObjectConfigSection from section name {sectionName}");
+
+            var keys = configSection.AllKeys;
+
+            var result = new T();
+
+            var properties = result.GetType().GetProperties().Where(x => x.CanWrite);
+
+            foreach (var propertyInfo in properties)
+            {
+                var propertyName = propertyInfo.Name;
+                if(keys.Any(x => string.Equals(x, propertyName, StringComparison.OrdinalIgnoreCase)))
+                {
+                    var value = configSection[propertyName];
+                    if (value != null)
+                    {
+                        // need to test
+                        //propertyInfo.SetValue(result, Convert.ChangeType(value, propertyInfo.PropertyType));
+                        propertyInfo.SetValue(result, value);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
